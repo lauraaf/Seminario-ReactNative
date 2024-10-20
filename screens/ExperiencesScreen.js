80435a32e3441f9e50fbb95d2486f557bba25a38
cbuzzz <sebpinsan12@gmail.com>
2024-10-20 15:09:28 +0200

    Depuración experiencias realizada

--- screens/ExperiencesScreen.js
+++ screens/ExperiencesScreen.js
@@ -21,7 +21,7 @@ export default function ExperiencesScreen() {
           new Experience(exp._id, exp.owner, exp.description, exp.participants)
       );
       setExperiences(experienceInstances);
-      console.log("Experiencias cargadas:", experiences);
+      console.log("Experiencias cargadas:", data);
     } catch (error) {
       console.error("Error al cargar experiencias:", error);
     }
