e5b1089192203b358ecb7ffbeedc93eebf84a703
cbuzzz <sebpinsan12@gmail.com>
2024-10-20 21:46:17 +0200

    Al crear una experiencia esta se añade al vector de experiencias de los usuarios participantes, pero al elimarla no lo elimina

--- services/experienceService.js
+++ services/experienceService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://10.0.2.2:3000/api/experiencias"; // Cambia a la URL correcta de tu API
+const API_URL = "http://10.0.2.2:3000/api/experiencias"; // URL correcta de tu API
 
 // Obtener todas las experiencias desde la API
 export const fetchExperiences = async () => {
@@ -17,7 +17,21 @@ export const fetchExperiences = async () => {
 export const addExperience = async (newExperience) => {
   try {
     const response = await axios.post(API_URL, newExperience);
-    return response.data; // Devolvemos los datos de la nueva experiencia
+    const nuevaExperiencia = response.data;
+
+    // Actualizar a los usuarios participantes con la nueva experiencia
+    const { participants } = newExperience;
+    if (participants && participants.length > 0) {
+      await Promise.all(
+        participants.map((userId) =>
+          // Usamos el método POST y pasamos los parámetros en la URL según la lógica del backend
+          axios.post(
+            `http://10.0.2.2:3000/api/user/addExperiencias/${userId}/${nuevaExperiencia._id}`
+          )
+        )
+      );
+    }
+    return nuevaExperiencia;
   } catch (error) {
     console.error("Error al agregar experiencia:", error);
     throw error;
@@ -30,7 +44,7 @@ export const deleteExperience = async (experienceId) => {
     if (!experienceId) {
       throw new Error("El ID de la experiencia es indefinido");
     }
-    await axios.delete(`${API_URL}/${experienceId}`); // Aquí nos aseguramos de que el _id se pase correctamente en la URL
+    await axios.delete(`${API_URL}/${experienceId}`); // Aseguramos que el _id se pase correctamente
   } catch (error) {
     console.error("Error al eliminar experiencia:", error);
     throw error;
